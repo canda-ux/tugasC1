b9fc09784b0cfa49ae16b3a14492ea5081673037
canda-ux <andikacanda7@gmail.com>
2021-05-30 19:49:47 +0700

    tugas mobile lanjutan C1

---
+++ app/src/main/java/com/andikacanda/recyclerviewb/MahasiswaModel.java
@@ -0,0 +1,26 @@
+package com.andikacanda.recyclerviewb;
+
+public class MahasiswaModel {
+
+    private String NIM, Nama,JenisKelamin;
+
+    public MahasiswaModel(){
+
+    }
+    public String getNIM(){return NIM;}
+
+    public void setNIM(String NIM) { this.NIM=NIM;}
+
+    public String getNama(){return Nama;}
+
+    public void setNama(String nama){this.Nama=nama;}
+
+    public String getJenisKelamin(){return JenisKelamin;}
+
+    public void setJenisKelamin(String jenisKelamin){this.JenisKelamin=jenisKelamin;}
+
+
+
+
+
+}
